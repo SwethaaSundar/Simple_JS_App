eefc5c706b40d4e15ce71b4bd8d570389a92ad81
SwethaaSundar <swethaasundar@gmail.com>
2023-02-01 14:53:22 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// variable names examples
+/* myname
+   my_name
+   myName - preffered
+   myName1*/
+
+// alert - displays the result on the pop up window
+//alert('Hello World') ;
+
+//console.log - display the result on console
+/*let firstName = "Swethaa";
+let lastName = "Sundar";
+console.log(firstName);
+console.log(lastName);*/
+
+//document.write - displays the result on the screen or 
+//on DOM  and it’ll do so in the exact place where it’s inserted in your HTML
+/*let firstName ="Swethaa";
+document.write(firstName);*/
+
+//Excecution from TOP to BOTTOM
+/* let myName ="Swethaa";
+document.write(myName);
+myName ="Swethaa Sundar";
+document.write(myName); */
+
+// Math expressions
+/*let simpleMath = 2+2; //2-2 2*2 2/2
+document.write(simpleMath);*/
+
+// -------------------------------------------------
+
+// Displays the output in the pop-up window
+alert("Hello World");
+
+let myFavFood ="Pizza";
+document.write(myFavFood);
