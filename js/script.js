c96dfc2f1603c3a682730dea7199ad695ad80116
SwethaaSundar <swethaasundar@gmail.com>
2023-02-07 15:46:14 +0100

    Ex-1.5 IIFE

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ document.write(myFavFood);*/
 
 // Ex-1.2
 // new variable pokemonList and adding obects to the array
-let pokemonList= [
+/*let pokemonList= [
     {name: "Pikachu" , height: 0.4 , 
     types: 'electric', weight: 6 },
 
@@ -39,8 +39,52 @@ let pokemonList= [
 
     {name: "Metagross", height: 1.6 , 
     types: ['psychic' , 'steel'] , weight: 550}
-];
+];*/
 
+let pokemonRepository= (function() {
+    let pokemonList= [
+        {name: "Pikachu" , height: 0.4 , 
+        types: 'electric', weight: 6 },
+    
+        {name: "Bulbasaur", height: 0.7 , 
+        types: ['grass','poison'], weight: 6.9 },
+    
+        {name: "Ponyta", height: 1 , 
+        types: 'fire', weight: 30},
+    
+        {name: "Butterfree", height: 1.1 , 
+        types: ['bug' , 'fly'] , weight: 32},
+    
+        {name: "Squirtle", height: 0.5 , 
+        types: 'water' , weight: 9},
+    
+        {name: "Nidoking", height: 1.4 , 
+        types: ['ground' , 'poison'] , weight: 62},
+    
+        {name: "Wigglytuff", height: 1 , 
+        types: ['fairy' , 'normal'] , weight: 12},
+    
+        {name: "Kangaskhan", height: 2.2 , 
+        types: 'normal' , weight: 80},
+    
+        {name: "Poliwrath", height: 1.3 , 
+        types: ['water' , 'fighting'] , weight: 54},
+    
+        {name: "Metagross", height: 1.6 , 
+        types: ['psychic' , 'steel'] , weight: 550}
+    ] 
+
+function add(list) {
+    pokemonList.push(list);
+  }
+function getAll(){
+    return pokemonList;
+}
+return {
+    add: add,
+    getAll: getAll
+  };
+})();
 // console.log(pokemonList[5]);
 // console.log(pokemonList[2].weight);
 // console.log(pokemonList[3].types[1]);
@@ -61,7 +105,8 @@ let pokemonList= [
 
 //Ex-1.5
 // part-1
-pokemonList.forEach (function(list) {
+// pokemonList.forEach (function(list) {
+pokemonRepository.getAll().forEach (function(list) {
     if (list.height < 1){
         document.write(list.name + " (height: " + list.height + " weight: " + list.weight + ")- Too small!!" + "<br><br>");
     }
