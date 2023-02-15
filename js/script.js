5337f151d3ed5762e4c2a2d0fde61603040a2773
SwethaaSundar <swethaasundar@gmail.com>
2023-02-15 16:02:36 +0100

    implement a modal with pokemon details

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ function addListItem(pokemon){
 
     button.innerText = pokemon.name;
     button.classList.add('button');
+    listItem.classList.add('list');
     pokeList.appendChild(listItem);
     listItem.appendChild(button);
 
@@ -23,15 +24,14 @@ function addListItem(pokemon){
 }
 function showDetails(pokemon){  
 loadDetails(pokemon).then(function () {
+  showModal(pokemon);
 });
 }
 
 function loadList() {
-  showLoadingMessage();
     return fetch(pokeapiUrl).then(function (response) {
       return response.json();
     }).then(function (json) {
-      hideLoadingMessage();
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
@@ -40,35 +40,90 @@ function loadList() {
         add(pokemon);
       });
     }).catch(function (e) {
-      hideLoadingMessage();
       console.error(e);
     })
   }
 
   function loadDetails(item) {
-    showLoadingMessage();
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      hideLoadingMessage();
       // Now we add the details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
       item.weight = details.weight;
     }).catch(function (e) {
-      hideLoadingMessage();
       console.error(e);
     });
   }
-  function showLoadingMessage(){
-    console.log("Loading details");
-  }
-  function hideLoadingMessage(){
-    console.log(" ");
-  }
-
+  function showModal(item){
+    let container = document.querySelector('.container');
+    container.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    // add button with class modal-close
+    let closeBtn = document.createElement('button');
+    closeBtn.classList.add('modal-close');
+    closeBtn.innerText = 'Close';
+    // hide when close button is clicked
+    closeBtn.addEventListener('click', hideModal);
+    // add title 
+    let modalTitle = document.createElement('h1');
+    modalTitle.classList.add('modal-title');
+    modalTitle.innerText= item.name;
+    // add type
+    let modalTypes = document.createElement('p');
+    modalTypes.classList.add('modal-types');
+    var pokeType = "";
+    for (let i in item.types) {
+        pokeType += item.types[i].type.name + " ";
+    }
+    modalTypes.innerText = "Types: " + pokeType;
+    // add height
+    let modalHeight = document.createElement('p');
+    modalHeight.classList.add('modal-height');
+    modalHeight.innerText = "Height: " + item.height + " cm";
+    // add weight
+    let modalWeight = document.createElement('p');
+    modalWeight.classList.add('modal-weight');
+    modalWeight.innerText = "Weight: " + item.weight + " kg";   
+    // add image 
+    let modalImage = document.createElement("img");
+    modalImage.classList.add('modal-image')
+    modalImage.setAttribute("src", item.imageUrl); 
+    // append all the element
+    modal.appendChild(closeBtn);
+    modal.appendChild(modalTitle);
+    modal.appendChild(modalTypes);
+    modal.appendChild(modalHeight);
+    modal.appendChild(modalWeight);
+    modal.appendChild(modalImage);
+    container.appendChild(modal);
+    // making the container visible
+    container.classList.add('is-visible');
+    // hide when clicked outside the container
+    container.addEventListener('click',(e) =>{
+    //   // Since this is also triggered when clicking INSIDE the modal
+    //   // We only want to close if the user clicks directly on the overlay
+      // let target = e.target;
+      if (e.target === container) {
+        hideModal();
+      }
+    });
+}
+  function hideModal(){
+    let container = document.querySelector('.container');
+    container.classList.remove('is-visible');
+}
+ // hide when esc key is pressed
+ window.addEventListener('keydown',(e) =>{
+ let container = document.querySelector('.container');
+ if (e.key === 'Escape' && container.classList.contains('is-visible')){
+ hideModal();
+ }
+});
 return {
     add: add,
     getAll: getAll,
@@ -76,8 +131,8 @@ return {
     showDetails: showDetails,
     loadList: loadList,
     loadDetails: loadDetails,
-    showLoadingMessage: showLoadingMessage,
-    hideLoadingMessage: hideLoadingMessage
+    showModal: showModal,
+    hideModal: hideModal
   };
 })();
 pokemonRepository.loadList().then(function() {
